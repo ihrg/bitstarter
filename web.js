faaf4dbd8bff40bb880f84eaa2175323694c8bbb
Ivica Hrg <hrgivica@gmail.com>
2013-11-15 14:21:11 +0000

    modifiy web.js

--- web.js
+++ web.js
@@ -4,12 +4,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var buffer = new Buffer("Hello World from index.html", 25);
-fs.readFileSync("index.html", "utf-8");
-response.send(resultOfreadFileSync);
+    var buffer = new Buffer("Hello World from index.html", 25);
+    fs.readFile('index.html', 'utf8', function (err,data) {
+	if (err) {
+	    return console.log(err);
+	}
+	response.send(data);
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
