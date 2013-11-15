71ea3b85292d37554675aff97c36be71c14d655c
Ivica Hrg <hrgivica@gmail.com>
2013-11-15 14:07:06 +0000

    web.js reads index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
+var fs = rewuire('fs');
 var express = require('express');
-var app = express();
-app.use(express.logger());
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = new Buffer("Hello World from index.html", 25);
+fs.readFileSync("index.html", "utf-8");
+response.send(resultOfreadFileSync);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
